eef0e52812f39dcd52e07ce7273a48334bd93e3b
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-17 21:13:50 +0530

    [gurpreet] Functions

---
+++ BasicJS/Functions.js
@@ -0,0 +1,43 @@
+
+//-----------let--------------
+// let a;
+// let b;
+// demo();
+// function demo(a,b) {
+//   let add = a+b;
+//   console.log(add);
+//   return add;
+// }
+// demo(10,20);
+
+
+
+//-----------var-------------
+// var x = 3;
+// var y = 2;
+// function multiply(x,y)
+// {
+//   return x*y;
+// }
+// console.log(multiply(x,y));
+
+
+
+//----------const-------------
+// const x = 7;
+// const y = 2;
+// function multiply(x,y)
+// {
+//   return x*y;
+// }
+// console.log(multiply(x,y));
+
+
+//---------arrow function----------
+let multiply = (a, b) => {
+    let result = a * b;
+    return result;
+  }
+  
+  let result1 = multiply(5,7);
+  console.log(result1); 
