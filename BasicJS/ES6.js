a8d4e39e9962cfbc3dfb8a3cb89fa082ac636c68
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-17 21:16:46 +0530

    [gurpreet] ES6

---
+++ BasicJS/ES6.js
@@ -0,0 +1,44 @@
+//features of ES6 
+
+//1. Default Function----------------------
+// const test = (a='a',b='b',c='c')=>{
+//     return a+b+c
+// }
+ 
+// console.log(test('A','B','C')) 
+// console.log(test('A','B'))     
+// console.log(test('A'))         
+
+
+
+// // 2. Template string-------------------
+//  let firstName = "Gurpreet";
+//  let lastName = "Singh";
+// console.log(`My name is ${firstName} ${lastName}.`);
+
+
+
+//-3. Spread Operator---------------------
+// var arr1 = [0, 1, 9];
+// var arr2 = [5, 7, 3];
+// var arr3 = [...arr1, ...arr2];// Append all elements in arr2 after arr1 and return
+// //Equate to
+// var arr4 = arr1.concat(arr2);
+// console.log(arr4)
+
+
+
+//4. Arrow function--------------------
+// const func = (a, b) => a + b;
+// func(1, 2); 
+// console.log(func(1, 2) );
+
+
+//5. Destructuring----------------------
+// let arr = ["Ajay", "Singh"]
+// let [firstName, surname] = arr;
+// console.log("FirstName: "+firstName);  
+// console.log("Surname: "+surname);  
+ 
+
+
