9d8fda3336d355a0e5cbf27c49b82664bb2d5a34
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-17 21:18:13 +0530

    [gurpreet] ES7

---
+++ BasicJS/ES7.js
@@ -0,0 +1,19 @@
+//features of ES7
+
+//1. includes()---------------------------------
+// let arr = ['Cat', 'Dog', 'Bird'];
+ 
+// if (arr.includes('Dog'))
+// {
+//     console.log('Exists');
+// }
+// else
+// {
+//     console.log('Not Exists');
+// }
+
+
+
+//2. Index Operator  ** ------------------------
+console.log(3**10);
+console.log(Math.pow(3, 10)) 
