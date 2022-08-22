f990e9381bad71f9564e66a54821ed3cde82272f
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-22 21:12:01 +0530

    [gurpreet] PushAndpop

---
+++ BasicJS/pushandpop.js
@@ -0,0 +1,12 @@
+//1. POP----------------------------------------------
+
+// var intArr = [23, 45, 67];
+// intArr.pop();    // returns the removed item
+// console.log(intArr);            //OUTPUT: [ 23, 45 ]
+
+
+
+//2. PUSH----------------------------------------------
+var intArr = [23, 45];
+intArr.push(67); // returns the new array length
+console.log(intArr);               //OUTPUT: [ 23, 45, 67 ]
